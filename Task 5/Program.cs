d27f6e39212241535cd8421ae64d95eb348df9e0
Mikhail <mehan@list.ru>
2022-09-25 15:18:35 +0300

    Done

--- Task 5/Program.cs
+++ Task 5/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Определите, является ли число степенью двойки:
+// N = 16 -> "Является степень двойки"
+// N = 12 -> “Не является степенью двойки”
+bool Check(int a)
+{
+    if (a % 2 == 1)
+    {
+        return false;
+    }
+    if (a == 2)
+    {
+        return true;
+    }
+
+    return Check(a / 2);
+}
+bool result = Check(28);
+System.Console.WriteLine(result);
+
