520766145a775af57ea220dabd032a698a92dbdd
Mikhail <mehan@list.ru>
2022-09-28 20:10:02 +0300

    Done

--- Homework/Task 1/Program.cs
+++ Homework/Task 1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int Prompt(string messege)
+{
+    Console.Write(messege);
+    string strValue = Console.ReadLine() ?? "0";
+    bool isNumber = int.TryParse(strValue, out int value);
+    if (isNumber)
+    {
+        return value;
+    }
+    throw new Exception("Данное значение не возможно преобразовать в число");
+}
+
+void ShowNumbers(int number)
+{
+    if (number == 0){return;}
+    System.Console.Write($"{number} ");
+    ShowNumbers(number - 1);
+
+}
+int digit = Prompt("Введите число: ");
+ShowNumbers(digit);
